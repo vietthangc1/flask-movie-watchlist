bb0aa2a77cef6a3a59d723474e929f68e83e2e78
vietthangc1 <pvthang1700@gmail.com>
2022-08-06 14:55:38 +0700

    chang to blue

--- movie_library/__init__.py
+++ movie_library/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     app = Flask(__name__)
     app.config["MONGODB_URI"] = "mongodb+srv://vietthangc1:"+urllib.parse.quote_plus('f2bdx@*-uLAZz!f')+"@cluster0.le7ea.mongodb.net/test"
     app.config["SECRET_KEY"] = os.environ.get(
-        "SECRET_KEY", "pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw"
+        "SECRET_KEY", "pf9Wkove4IKEAXvy-cQkeDohv9Cb3Ag-wyJILbq_dFw"
     )
     app.db = MongoClient(app.config["MONGODB_URI"])["movie-watchlist"]
 
